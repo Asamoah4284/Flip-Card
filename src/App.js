3a6b519a67bf49dd499d951b5af83ad0bd9684ab
Asamoah4284 <richardasamoah284@gmail.com>
2024-02-05 08:42:36 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,154 +1,53 @@
-// import { useState } from "react";
-
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: true },
-//   { id: 3, description: "Charger", quantity: 1, packed: false },
-// ];
-
-// export default function App() {
-//   return (
-//     <div className="app">
-//       <Logo />
-//       <Form />
-//       <PackingList />
-//       <Stats />
-//     </div>
-//   );
-// }
-
-// function Logo() {
-//   return <h1>🌴Far Away💼</h1>;
-// }
-// function Form() {
-//   const [description, setDescription] = useState("");
-//   const [quantity, setQuantity] = useState(1);
-//   function handleSubmit(e) {
-//     e.preventDefault();
-//     if (!description) return;
-//     const newItem = { description, quantity, packed: false, id: Date.now() };
-//     console.log(newItem);
-
-//     setDescription("");
-//     setQuantity(1);
-//   }
-//   return (
-//     <form className="add-form" onSubmit={handleSubmit}>
-//       <h3>What do you need for your trip🥰</h3>
-//       <select
-//         value={quantity}
-//         onChange={(e) => setQuantity(Number(e.target.value))}
-//       >
-//         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-//           <option value={num} key={num}>
-//             {num}
-//           </option>
-//         ))}
-//       </select>
-//       <input
-//         type="text"
-//         placeholder="Item..."
-//         value={description}
-//         onChange={(e) => setDescription(e.target.value)}
-//       />
-//       <button>Add</button>
-//     </form>
-//   );
-// }
-// function PackingList() {
-//   return (
-//     <div className="list">
-//       <ul>
-//         {initialItems.map((item) => (
-//           <Item item={item} key={item.id} />
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-// function Item({ item }) {
-//   return (
-//     <li>
-//       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-//         {item.quantity} {item.description}
-//       </span>
-//       <button>❌</button>
-//     </li>
-//   );
-// }
-// function Stats() {
-//   return (
-//     <footer>
-//       <em>💼You have X items on yor list and you already packed X (X%)</em>
-//     </footer>
-//   );
-// }
-
 import { useState } from "react";
 import "./index.css";
 
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
 export default function App() {
   return (
-    <div className="App">
-      <FlashCards />
+    <div>
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-const questions = [
-  {
-    id: 3457,
-    question: "What language is React based on?",
-    answer: "JavaScript",
-  },
-  {
-    id: 7336,
-    question: "What are the building blocks of React apps?",
-    answer: "Components",
-  },
-  {
-    id: 8832,
-    question: "What's the name of the syntax we use to describe a UI in React?",
-    answer: "JSX",
-  },
-  {
-    id: 1297,
-    question: "How to pass data from parent to child components?",
-    answer: "Props",
-  },
-  {
-    id: 9103,
-    question: "How to give components memory?",
-    answer: "useState hook",
-  },
-  {
-    id: 2002,
-    question:
-      "What do we call an input element that is completely synchronised with state?",
-    answer: "Controlled element",
-  },
-];
-
-function FlashCards() {
-  const [selectedId, setSelectedId] = useState(null);
-
-  function handleClick(id) {
-    setSelectedId(id !== selectedId ? id : null);
-  }
-  console.log(selectedId);
+function Accordion({ data }) {
   return (
-    <div className="flashcards">
-      {questions.map((question) => (
-        <div
-          key={question.id}
-          onClick={() => handleClick(question.id)}
-          className={question.id === selectedId ? "selected" : ""}
-        >
-          <p>
-            {question.id === selectedId ? question.answer : question.question}
-          </p>
-        </div>
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} key={i} />
       ))}
     </div>
   );
 }
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
+}
+
+// Tip calculator App
